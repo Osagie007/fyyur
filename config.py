5cb2803301880c5bf4a91e0bfe630ac245ec300e
Osagie007 <67745253+Osagie007@users.noreply.github.com>
2022-09-08 23:12:55 +0100

    first commit

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Iloveosagie@localhost:5432/fyyurdb'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432/fyyurdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
